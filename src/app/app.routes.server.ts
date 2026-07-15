0be91fa1741fd9e74eb16654a0628d4a6b5dda58
rafek-awny11 <rafekawny8@gmail.com>
2026-07-15 04:10:42 +0300

    render3

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,29 +1,6 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: 'studentDetails/:slug /:id',
-    renderMode: RenderMode.Server
-  },  
-  {
-    path: 'update-meeting/:slug /:id',
-    renderMode: RenderMode.Server
-  },
-  {
-    path:'show-details/:slug /:id' ,
-    renderMode: RenderMode.Server
-  },
-    {
-    path: 'details-meeting/:slug /:id',
-    renderMode: RenderMode.Server
-  },  {
-    path: 'details/:slug/:id',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'update/:slug /:id',
-    renderMode: RenderMode.Server
-  },
   
   {
     path: '**',
